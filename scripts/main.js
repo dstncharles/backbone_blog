6bf8b098518db2568c8efc6ee7f346d03a251742
Dustin Morando <dustinmorando@me.com>
2015-02-16 15:44:21 -0500

    commented sections and linked JS to HTML

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+(function(){
+/////////////
+//Model
+/////////////
+var BlogModel = Backbone.Router.extend({
+	
+});
+
+/////////////
+//View
+/////////////
+var BlogPostView = Backbone.View.extend({
+	
+});
+
+/////////////
+//Router
+/////////////
+var BlogRouter = Backbone.Router.extend({
+
+});
+})();
